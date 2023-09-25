d4a469d79d6015cf479016215a94ee40302b20e6
Charles <Charlessammarco@gmail.com>
2023-09-24 22:13:33 -0500

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState,useEffect} from 'react';
 
 function App() {
+
+const [getClassName, setGetClassName] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:3000/class")
+      .then((r) => r.json())
+      .then((items) => {
+        items.map((item)=> {
+          setGetClassName(item.name)
+        })
+        console.log(getClassName)
+      }
+      
+      );
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+
+
+        <a>
           Hello World
         </a>
-      </header>
+  
     </div>
   );
 }
